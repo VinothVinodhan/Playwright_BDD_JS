3574dc770cb09c60469fc7e234021efe24d69337
VinothKumar <vinothkumar280@gmail.com>
2025-10-18 15:46:01 +0530

    Cleanup

--- features/step_definitions/automationexercise_stepDef.js
+++
@@ -1,6 +0,0 @@
-const {Given, When, Then}= require('@cucumber/cucumber');
-
-Given('User navigates to {string}', async function (appUrl) {
-    console.log(`Navigating to URL: ${appUrl}`);
-    await this.page.goto(appUrl);
-});
